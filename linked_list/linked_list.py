c1641e8e50f9b1953cb9e753ed2037bf0fb83504
Victor Almeida Morais <vamdez123@gmail.com>
2022-10-19 12:00:36 -0300

    add function pop and multiple value

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,33 +1,71 @@
 class Node:
-    def __init__(self, value, next_node = None):
-        self.value = value
-        self.next_node = next_node
-
-    def value(self):
-        pass
-
-    def next(self):
-        pass
+    def __init__(self, values = None, next = None):
+        self.value = values
+        self.next = next
 
 
 class LinkedList:
     def __init__(self, values=[]):
         self.head = None
-        self.tail = None
-        if values:
-            self.add_multiples_nodes(values)
+        self.values = values
+
+
+    def insert_at_begining(self, values):
+        node = Node(values, self.head)
+        self.head = node
+
+    def insert_at_end(self, values):
+        if self.head is None:
+            self.head = Node(values, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(values, None)
+    
+    def insert_multiples_values(self, list_values):
+        for value in list_values:
+            self.insert_at_end(value)
+
+    def print(self):
+        if self.head is None:
+            print('Linked List is empty')
+            return
+        itr = self.head
+        valuestr = ''
+        while itr:
+            valuestr += str(itr.value) + ' --> '
+            itr = itr.next
+        print(valuestr)
 
     def __len__(self):
-        pass
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        print(count)
 
-    def head(self):
-        pass
+    def header(self):
+        if self.head is None:
+            print('Linked List is empty')
+            return
+        print(str(self.head.value))
 
-    def push(self, value):
-        pass
-
-    def pop(self):
-        pass
+    def pop(self, value_pop):
+        if value_pop == self.head.value:
+            self.head = self.head.next
+            return
+        itr = self.head
+        count = 0
+        while itr.value != value_pop:
+            itr = itr.next
+            count += 1
+        itr = self.head
+        while count > 1:
+            count -= 1
+            itr = itr.next
+        itr.next = itr.next.next
 
     def reversed(self):
         pass
@@ -35,3 +73,12 @@ class LinkedList:
 
 class EmptyListException(Exception):
     pass
+
+
+ll = LinkedList()
+ll.insert_at_begining(13)
+ll.insert_multiples_values([10,20,30,40,50,60,70,80,90])
+ll.pop(60)
+ll.pop(80)
+ll.__len__()
+ll.print()
