dd7ff9cf663435712bb65bceebe6e0c9cda33cd1
Victor Almeida Morais <vamdez123@gmail.com>
2023-02-15 17:58:41 -0300

    resistor_color_trio finish

--- resistor_color_trio/resistor_color.py
+++ resistor_color_trio/resistor_color.py
@@ -1,2 +1,18 @@
+colors_values = {"black": "0", "brown": "1", "red": "2", "orange": "3", "yellow": "4",
+                "green": "5", "blue": "6", "violet": "7", "grey": "8", "white": "9"}
+
+metric_prefix = ["", "kilo", "mega", "giga", "tera"]
+
+
 def label(colors):
-    pass
+    resp = str(int(colors_values[colors[0]] + colors_values[colors[1]]))
+    resp += ("0" * int(colors_values[colors[2]]))
+    count = 0
+    while True:
+        if resp[-3:] == "000":
+            resp = resp[0:-3]
+            count += 1
+        else:
+            return resp + " " + metric_prefix[count] + "ohms"
+
+print(label(["yellow", "violet", "yellow"]))
